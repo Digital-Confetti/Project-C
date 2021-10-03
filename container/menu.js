806374964a9e203f5d1e2c88d3a66c91a7cac246
Miguel Rodriguez <maiguelonflis456@gmail.com>
2021-10-03 18:01:31 +0200

    Primera version menu

--- container/menu.js
+++ container/menu.js
@@ -1,23 +1,40 @@
-export class Menu_Scene extends Phaser.Scene{
+export class Menu_Scene extends Phaser.Scene {
 
-    constructor()
-    {
-        super({ key: 'menu_Scene'});
+    constructor() {
+        super({ key: 'menu_Scene' });
+
+        this.background;
+        this.text;
+        this.pointer;
     }
 
     // Here we need to load all the graphics
-    preload(){
+    preload() {
         console.log('Menu Escena');
 
-        this.scene.launch("game_Scene");
+        //this.scene.launch("game_Scene");
+
+        this.load.image('fondo', 'stores/bluemenu.jpg');
     }
 
     // Here we need to create all the Modules
-                                        //^^^---Like player, platform, Pwr_Up..
-    create(){
+    //^^^---Like player, platform, Pwr_Up..
+    create() {
+        this.background = this.add.image(0, 0, 'fondo');
+        this.background.setScale(1.5);
+
+        this.text = this.add.text(270, 360, 'Pulsa en cualquier lugar para continuar', { color: '#000000', fontSize: '50px', fontFamily: 'Gemunu Libre'});
+
+        var that = this;
+        this.input.on('pointerdown', function(pointer){
+            console.log('Menu a Game');
+            that.scene.start("game_Scene");
+        });
     }
 
-    update()
-    {
+    update() {
+        this.pointer = this.input.activePointer;
+
+        //console.log(this.pointer.x, this.pointer.y);
     }
 }
