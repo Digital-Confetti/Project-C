e5416ec819051ae213621c454571297fcaa1d465
Javier Raja <iivaj@icloud.com>
2021-10-17 16:46:35 +0200

    .json atlas update

--- container/scripts/player/grundlegend.js
+++ container/scripts/player/grundlegend.js
@@ -2,7 +2,7 @@ import { Player } from "./player.js";
 
 export class GrundLegend extends Player{
     constructor(scene, x, y){
-        super(scene, x, y, 'byConfetti');
+        super(scene, x, y, 'GrundLegend');
 
         this.body.setBounce(0.1);
         this.body.setOffset(11, 0);
--- container/scripts/scenes/game.js
+++ container/scripts/scenes/game.js
@@ -10,34 +10,9 @@ export class Game_Scene extends Phaser.Scene {
     constructor() {
         super({ key: 'game_Scene' });
 
-        // atribute declaration
         this.player;
-        // Play
-        this.aceleration = 3;
-        this.horizontalSpeed = 225;
-        this.verticalSpeed = 10;
 
-        // Input Variables
-        this.keyW = false;   // ^ Boolean Key Catchers
-        this.keyS = false;
-        this.keyA = false;
-        this.keyD = false;
-        this.keySPACE = false;
-        this.keySHIFT = false;
-
-        // Platforms
         this.platforms;
-
-        // moving
-        this.moving_R = false;
-        this.dash_R = false;
-        this.drag = 4.5;
-        this.dashForce = 800;
-                         // s -> ms
-        this.dashCoolDown = 3 * 1000;
-        this.dashAllowed = false;
-        this.dashActivated = false;
-
 
         // Timers
         this.timer_dash;
@@ -45,45 +20,28 @@ export class Game_Scene extends Phaser.Scene {
         // debug
         this.text_Debug;
 
-
-        // proto
-        const stateMachine = {
-            IDDLE: "iddle",
-            RIGHT: "right",
-            LEFT: "left",
-            JUMPING: "jumping",
-        }
+        // s -> ms
+        this.dashCoolDown = 3 * 1000;
 
     }
 
     preload() {
         // loading the spritesheet on 
 
-        this.load.spritesheet('byConfetti', 'stores/characters/by_Confetti.png', { frameWidth: 60, frameHeight: 84 });
+        this.load.atlas("GrundLegend", "stores/characters/a.png", "stores/characters/a.json");
+
+        console.log(this.textures)
+        //this.load.spritesheet('byConfetti', 'stores/characters/by_Confetti.png', { frameWidth: 60, frameHeight: 84 });
 
         this.load.spritesheet('dude', 'stores/characters/dude.png', { frameWidth: 32, frameHeight: 48 });
 
         this.load.image('ground', 'stores/schenery/platform.png');
-
-        //this.load.spritesheet('kennewsprites', 'src/sprites/kennewsprites.png', 76, 101, 63);
     }
 
     // Function thats add all the sprites to the gameObjects
     createGameObjects() {
+        // Creating the player
         this.player = new GrundLegend(this, 100, 100);
-/*
-        // Adding Sprite to the player
-        //this.player = this.physics.add.sprite(400, 250, 'byConfetti');
-        // Setting bounce
-        this.player.setBounce(0.1);
-        // Making player collideable by WorldBounds
-        this.player.setCollideWorldBounds(true);
-        // Displacing the hitbox
-        this.player.body.setOffset(11, 0);
-        // Setting Size of the collider box
-        this.player.body.setSize(33, 84, false);
-        //this.player.body.refreshBody();
-*/
 
         // Creating Platforms
         this.platforms = this.physics.add.staticGroup();
@@ -107,10 +65,10 @@ export class Game_Scene extends Phaser.Scene {
         // Iddle
         this.anims.create({
             key: 'idle',
-            frames: [{ key: 'dude', frame: 4 }],
-            frameRate: 20
+            frames: [{ key: 'GrundLegend', frame: "by_Confetti-0.png" }],
+            frameRate: -1
         });
-
+/*
         // Right
         this.anims.create({
             key: 'right',
@@ -124,6 +82,7 @@ export class Game_Scene extends Phaser.Scene {
             key: 'left',
             frameRate: 8
         });
+*/
     }
 
     timer_Create() {
@@ -141,7 +100,7 @@ export class Game_Scene extends Phaser.Scene {
         this.createGameObjects();
 
         // Creating animations
-        //this.createAnimations();
+        this.createAnimations();
 
         // Declarating input methods
         this.inputDeclaration();
@@ -178,99 +137,12 @@ export class Game_Scene extends Phaser.Scene {
 
     }
 
-    // NOT USED
-    /*
-    plyMove(delta)
-    {
-        // Horizontal movement
-        if (this.keyD && !this.keyA) {
-            if (this.player.body.velocity.x <= this.horizontalSpeed){
-                this.player.body.velocity.x += this.aceleration * delta;
-            }
-
-            //this.player.anims.play('right', true);
-            this.moving_R = true;       
-
-        } else if (this.keyA && !this.keyD) {
-            
-            if (this.player.body.velocity.x >= -1 * this.horizontalSpeed){
-                this.player.body.velocity.x -= this.aceleration * delta;
-            }
-
-            //this.player.anims.play('right', true);
-            this.moving_R = false;
-        
-        // drag force
-        } else if (!this.keyA && !this.keyD){
-            if (this.moving_R && this.player.body.velocity.x != 0)
-            {
-                this.player.body.velocity.x -= this.drag * delta;
-                
-                if (this.player.body.velocity.x <= 0)
-                {
-                    this.player.setVelocityX(0);
-                }
-            } else if (!this.moving_R && this.player.body.velocity.x != 0)
-            {
-                this.player.body.velocity.x += this.drag * delta;
-                
-                if (this.player.body.velocity.x >= 0)
-                {   
-                    this.player.setVelocityX(0);
-                }
-            }
-        }
-
-        // Vertical movement
-        // Jump
-        if (this.keySPACE && this.player.body.touching.down)
-        {
-            console.log('Salto');
-            this.setVelocityY(-430);
-        }
-
-        if(this.dashActivated)
-        {
-            if(this.player.body.touching.left)
-            {
-                this.dash_R = true;
-                this.moving_R = true;
-            } else if (this.player.body.touching.right)
-            {
-                this.dash_R = false;
-                this.moving_R = false;
-            }
-
-            if (this.dash_R)
-            {
-                this.player.body.velocity.x = this.dashForce;
-            } else {
-                this.player.body.velocity.x = -1 * this.dashForce;
-            }
-        }
-
-        if (this.timer_dash.paused && this.keySHIFT)
-        {
-            this.timer_dash.paused = false;
-            this.dashAllowed = false;
-            this.dashActivated = true;
-            this.dash_R = this.moving_R;
-        }
-
-        if(this.player.body.velocity.x > 0)
-        {
-            this.player.flipX = false;
-        } else if (this.player.body.velocity.x < 0){
-            this.player.flipX = true;
-        }
-    }
-    */  
-
     update(timer, delta) {
 
         this.timer_Update();
 
         this.player.update(delta);
+        this.player.play('idle');
 
         var out;
 
@@ -306,6 +178,12 @@ export class Game_Scene extends Phaser.Scene {
             } else if (event.keyCode == Phaser.Input.Keyboard.KeyCodes.SHIFT) {
                 that.keySHIFT = true;
                 console.log('SHIFT Pressed');
+            } else if (event.keyCode == Phaser.Input.Keyboard.KeyCodes.J) {
+                that.keyNA = true;
+                console.log('J Pressed');
+            } else if (event.keyCode == Phaser.Input.Keyboard.KeyCodes.K) {
+                that.keySA = true;
+                console.log('K Pressed');
             }
 
         });
@@ -331,8 +209,40 @@ export class Game_Scene extends Phaser.Scene {
             } else if (event.keyCode == Phaser.Input.Keyboard.KeyCodes.SHIFT) {
                 that.keySHIFT = false;
                 console.log('SHIFT Depressed');
+            } else if (event.keyCode == Phaser.Input.Keyboard.KeyCodes.J) {
+                that.keyNA = false;
+                console.log('J Depressed');
+            } else if (event.keyCode == Phaser.Input.Keyboard.KeyCodes.K) {
+                that.keySA = false;
+                console.log('K Depressed');
             }
+        });
 
+        // Mouse Input
+        this.input.mouse.disableContextMenu();
+
+        this.input.on('pointerdown', function (event) {
+            if (event.rightButtonDown())
+            {
+                that.keySA = true;
+                console.log('RClick Pressed');
+            } else if (event.leftButtonDown()) {
+                that.keyNA = true;
+                console.log('LClick Pressed');
+            }
+        });
+
+        this.input.on('pointerup', function (event) {
+            if (event.leftButtonReleased())
+            {
+                that.keySA = false;
+                console.log('LClick Deressed');
+            }
+            else if (event.rightButtonReleased())
+            {
+                that.keySA = false;
+                console.log('RClick Depressed');
+            }
         });
     }
 }
